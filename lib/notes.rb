a50a0019719d58a700b91d8ac314d4b721f5260d
jebax <joel.eliot.baxter@gmail.com>
2018-10-04 12:16:36 +0100

    wrote first test

--- lib/notes.rb
+++ lib/notes.rb
@@ -1,2 +1,4 @@
 class Notes
+
+
 end
--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -1,5 +1,13 @@
 require 'notes'
 
 describe Notes do
+  let(:notes) { subject }
 
+  describe "#add" do
+    it 'should add a note to the collection of notes' do
+      title, body = "My first note", "Pick up milk on the way home"
+      notes.add(title, body)
+      expect(notes.all_notes).not_to be_empty
+    end
+  end
 end
