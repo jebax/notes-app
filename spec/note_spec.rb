27331de72d4b7b35313397cfd797e5a045c7acb2
jebax <joel.eliot.baxter@gmail.com>
2018-10-05 11:23:29 +0100

    removed let statements

--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -2,19 +2,23 @@ require 'notes'
 require 'pry'
 
 describe Notes do
-  let(:notes) { subject }
-  let(:add_test_note) { notes.add("Note 1", "Body") }
-  let(:add_second_note) { notes.add("Note 2", "Body") }
+  def add_test_note
+    subject.add("Note 1", "Body")
+  end
 
-  it 'should add a note to the list then print it out' do
+  def add_second_note
+    subject.add("Note 2", "Body")
+  end
+
+  it 'should add a note to the list then print its title' do
     add_test_note
-    expect { notes.titles_list }.to output("Your notes:\nNote 1\n").to_stdout
+    expect { subject.titles_list }.to output("Your notes:\nNote 1\n").to_stdout
   end
 
   describe "#add" do
     it 'should raise an error if the note title already exists' do
       add_test_note
-      expect { notes.add("Note 1", "Body") }.to raise_error "Note title already exists!"
+      expect { subject.add("Note 1", "Body") }.to raise_error "Note title already exists!"
     end
   end
 
@@ -22,11 +26,11 @@ describe Notes do
     it 'should list multiple note titles' do
       add_test_note
       add_second_note
-      expect { notes.titles_list }.to output("Your notes:\nNote 1\nNote 2\n").to_stdout
+      expect { subject.titles_list }.to output("Your notes:\nNote 1\nNote 2\n").to_stdout
     end
 
     it 'should raise an error if there are no stored notes' do
-      expect { notes.titles_list }.to raise_error "There are no notes to print!"
+      expect { subject.titles_list }.to raise_error "There are no notes to print!"
     end
   end
 
@@ -34,12 +38,12 @@ describe Notes do
     it 'should print a note from a given title' do
       add_test_note
       add_second_note
-      expect(notes.print_specific_note("Note 1")).to eq "Note 1: Body"
+      expect(subject.print_specific_note("Note 1")).to eq "Note 1: Body"
     end
 
     it 'should raise an error if the specified note does not exist' do
       add_test_note
-      expect { notes.print_specific_note("Note 2") }.to raise_error "Specified note does not exist!"
+      expect { subject.print_specific_note("Note 2") }.to raise_error "Specified note does not exist!"
     end
   end
 end
